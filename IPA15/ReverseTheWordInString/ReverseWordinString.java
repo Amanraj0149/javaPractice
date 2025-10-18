5cbf0b27fcf1b1f980bd29b081f4c647d832a316
Aman Raj <135144595+Amanraj0149@users.noreply.github.com>
2025-10-18 23:23:07 +0530

    Update ReverseWordinString.java

--- IPA15/ReverseTheWordInString/ReverseWordinString.java
+++ IPA15/ReverseTheWordInString/ReverseWordinString.java
@@ -10,3 +10,16 @@ class Solution {
         return res.trim();
     } 
 }
+
+
+// tc:-0(n)
+class Solution {
+    public String reverseWords(String s) {
+        String arr[]=s.split("\\s+");
+         StringBuilder res= new StringBuilder();
+        for(int i=0;i<arr.length;i++){                                                                        // aman ka naam 
+           res.append(new StringBuilder(arr[i]).reverse()).append(" ");                                     // o/p: nama ak maan
+        }
+        return res.toString().trim();
+    } 
+}
